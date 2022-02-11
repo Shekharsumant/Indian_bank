3915a993ab85fc24ce35bb10c66557a31dd2491c
Shekharsumant <90973794+Shekharsumant@users.noreply.github.com>
2022-02-11 18:56:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="StyleSheet1.css" />
+    <title></title>
+</head>
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <span class="fs-4">INDIAN BANK</span>
+            </a>
+
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="createuser.php" class="nav-link">Create a User</a></li>
+                <li class="nav-item"><a href="transfermoney.php" class="nav-link">Transfar Money</a></li>
+                <li class="nav-item"><a href="transactionhistory.php" class="nav-link">Transaction History</a></li>
+            </ul>
+
+        </header>
+        <h1 class="d-flex flex-wrap justify-content-center"><span>Welcome To INDIAN BANK</span></h1>
+        <h3 class="d-flex flex-wrap justify-content-center">"Build A Better Life Around Us"</h3>
+
+        <img src="img\th 1.jpg" class="one" width="180px" height="180px" />
+        <a href="createuser.php"><button class="btn">Create a User</button></a>
+
+        <img src="img\th 5.jpg" class="two" width="180px" height="180px" />
+        <a href="transfermoney.php"> <button class="btn">Make Transaction</button></a>
+
+        <img src="img\th 6.jpg" class="three" width="180px" height="180px" />
+        <a href="transactionhistory.php"><button class="btn">Transaction History</button></a>
+
+    </div>
+
+</body>
+</html>
